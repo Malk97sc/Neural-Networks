4a8c7d94d8164198283d647d0541b396ea9b0d1a
Malak <malaksanchez09@gmail.com>
2026-04-01 11:07:02 -0500

    Parallel Header

---
+++ include/parallel.h
@@ -0,0 +1,12 @@
+#ifndef PARALLEL_H
+#define PARALLEL_H
+
+#include "matrix.h"
+
+void matvec_parallel_impl(const Matrix *A, const float *x, float *y, int n_threads);
+void matmul_parallel_impl(const Matrix *A, const Matrix *B, Matrix *C, int n_threads);
+
+void mat_add_rowwise_parallel_impl(Matrix *A, const float *b, int n_threads);
+void mat_apply_parallel_impl(Matrix *A, float (*func)(float), int n_threads);
+
+#endif
